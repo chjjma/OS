f09d8129ec5aebd69e8b915510db96877e23fa97
wooks1998 <44969806+wooks1998@users.noreply.github.com>
2019-04-03 22:58:54 +0900

    Add files via upload

--- syscall.c
+++ syscall.c
@@ -19,6 +19,49 @@ syscall_handler (struct intr_frame *f)
   thread_exit ();
 }
 
+/*Terminates Pintos by calling shutdown_power_off() 
+  (declared in devices/shutdown.h). 
+  This should be seldom used, 
+  because you lose some information about possible deadlock situations, etc.*/
+void halt (void){
+	//not implemented
+	return;
+}
+
+/*Terminates the current user program, returning status to the kernel.
+  If the process's parent waits for it (see below),
+  this is the status that will be returned. Conventionally,
+  a status of 0 indicates success and nonzero values indicate errors.*/
+void exit (int status){
+	//not implemented
+	return;
+}
+
+/*Runs the executable whose name is given in cmd_line, passing any given arguments,
+  and returns the new process's program id (pid). Must return pid -1,
+  which otherwise should not be a valid pid, if the program cannot load or run for any reason.
+  Thus, the parent process cannot return from the exec until it knows
+  whether the child process successfully loaded its executable.
+  You must use appropriate synchronization to ensure this.*/
+pid_t exec (const char *cmd_line){
+	//not implemented
+	return 0;
+}
+
+/*Waits for a child process pid and retrieves the child's exit status.
+  If pid is still alive, waits until it terminates.
+  Then, returns the status that pid passed to exit.
+  If pid did not call exit(), but was terminated by the kernel 
+  (e.g. killed due to an exception), wait(pid) must return -1.
+  It is perfectly legal for a parent process to wait for child processes
+  that have already terminated by the time the parent calls wait,
+  but the kernel must still allow the parent to retrieve its child's exit status,
+  or learn that the child was terminated by the kernel.*/
+int wait (pid_t pid){
+	//not implemented
+  	return 0;
+}
+
 /* Creates a new file called file initially initial_size bytes in size. 
   Returns true if successful, false otherwise. 
   Creating a new file does not open it: opening the new file is 
