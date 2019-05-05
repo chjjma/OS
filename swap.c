0f0a043d537a3b8cd718824405e4f5eb18885e08
chjjma <44969639+chjjma@users.noreply.github.com>
2019-05-05 22:31:38 +0900

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,116 @@
+#include "vm/swap.h"
+#include "vm/page.h"
+#include "devices/disk.h"
+#include "threads/synch.h"
+#include <bitmap.h>
+
+
+/* The swap device */
+static struct disk *swap_device;
+
+/* Tracks in-use and free swap slots */
+static struct bitmap *swap_table;
+
+/* Protects swap_table */
+static struct lock swap_lock;
+
+/* 
+ * Initialize swap_device, swap_table, and swap_lock.
+ */
+
+int swapsize; 
+
+void 
+swap_init (void)
+{
+  swap_device=disk_get(1,1);
+  swapsize=DISK_SECTOR_SIZE*disk_size(swap_device)/PGSIZE;
+  swap_table=bitmap_create(disk_size(swapsize);
+  lock_init(swap_lock);
+}
+
+/*
+ * Reclaim a frame from swap device.
+ * 1. Check that the page has been already evicted. 
+ * 2. You will want to evict an already existing frame
+ * to make space to read from the disk to cache. 
+ * 3. Re-link the new frame with the corresponding supplementary
+ * page table entry. 
+ * 4. Do NOT create a new supplementray page table entry. Use the 
+ * already existing one. 
+ * 5. Use helper function read_from_disk in order to read the contents
+ * of the disk into the frame. 
+ */ 
+bool 
+swap_in (void *addr)
+{
+  struct sup_page_table_entry spte=find_page(addr);
+  ASSERT(spte->loc != 1);
+  if(!swap_out()) return false;
+  int index=(int)spte->phys_addr;
+  lock_acquire(swap_lock);
+	if(swap_table[index]==false) return false;
+  lock_release(swap_lock);
+  uint8_t *frame=allocate_frame(addr);
+  read_from_disk(frame, index);
+  lock_acquire(swap_lock);
+	swap_table[index]=false;
+  lock_release(swap_lock);
+  return true;
+}
+
+/* 
+ * Evict a frame to swap device. 
+ * 1. Choose the frame you want to evict. 
+ * (Ex. Least Recently Used policy -> Compare the timestamps when each 
+ * frame is last accessed)
+ * 2. Evict the frame. Unlink the frame from the supplementray page table entry
+ * Remove the frame from the frame table after freeing the frame with
+ * pagedir_clear_page. 
+ * 3. Do NOT delete the supplementary page table entry. The process
+ * should have the illusion that they still have the page allocated to
+ * them. 
+ * 4. Find a free block to write you data. Use swap table to get track
+ * of in-use and free swap slots.
+ */
+bool
+swap_out (void)
+{
+  uint8_t *frame=evict_frame();
+  if(frame==NULL)return false;
+  lock_acquire(swap_lock);
+   
+  int i;
+  for(i=0;i<swapsize;i++)if(swap_table[i]==false) break;
+  if(i==swapsize)PANIC();
+  swap_table[i]=true;
+  write_to_disk(frame,i);
+  find_frame(frame)->spte->phys_addr=i;
+  lock_release(swap_lock);
+  return true;
+}
+
+/* 
+ * Read data from swap device to frame. 
+ * Look at device/disk.c
+ */
+void read_from_disk (uint8_t *frame, int index)
+{
+  uint32_t ind=4*((uint32_t)find_frame(frame)->spte->phys_addr);
+  disk_read(swap_disk, ind, frame);
+  disk_read(swap_disk, ind+1, frame+DISK_SECTOR_SIZE);
+  disk_read(swap_disk, ind+2, frame+2*DISK_SECTOR_SIZE);
+  disk_read(swap_disk, ind+3, frame+3*DISK_SECTOR_SIZE);
+}
+
+/* Write data to swap device from frame */
+void write_to_disk (uint8_t *frame, int index)
+{
+  uint32_t ind=4*((uint32_t)find_frame(frame)->spte->phys_addr);
+  disk_write(swap_disk, ind, frame);
+  disk_write(swap_disk, ind+1, frame+DISK_SECTOR_SIZE);
+  disk_write(swap_disk, ind+2, frame+2*DISK_SECTOR_SIZE);
+  disk_write(swap_disk, ind+3, frame+3*DISK_SECTOR_SIZE);
+
+}
+
